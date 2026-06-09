be80b556bc0b52ef64c9bfbc00493b5c0d43386d
python3-trying-py <paytay2023@gmail.com>
2026-06-08 21:17:14 -0400

    Error Handling and Posts Added some error handling for bad urls and files.
    Imporved the displaying of posts

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,13 @@
 from flask import render_template
-from app import app
+from app import app, db
+from app.models import Post
+import sqlalchemy as sa
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Payton'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+
+    query = sa.select(Post).order_by(Post.id)
+    posts = db.session.scalars(query).all()
+
+    return render_template("index.html", title='Home Page', posts=posts)
--- main.py
+++ main.py
@@ -2,7 +2,26 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import Post
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'Post': Post}
+
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Blog startup')
